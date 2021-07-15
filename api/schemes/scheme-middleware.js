13930662f05c640708d2655970ddf4249118592a
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-07-14 21:46:05 -0500

    validates are constructed

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -32,7 +32,12 @@ const checkSchemeId = async (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+    const { scheme_name } = req.body
+    if (scheme_name === undefined || typeof scheme_name !== 'string' || !scheme_name.trim()) {
+      next({ status: 404, message: `invalid scheme_name` })
+    } else {
+      next()
+    }
 }
 
 /*
@@ -45,7 +50,16 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
+  const { instructions, step_number } = req.body
+  if (instructions === undefined || 
+    typeof instructions !== "string" || 
+    !instructions.trim() || 
+    typeof step_number !== 'number' || 
+    step_number < 1) {
+      next({ status: 404, message: `invalid step` })
+    } else {
+      next()
+    }
 }
 
 module.exports = {
